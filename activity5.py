5a92df50924a06a2f461c9bb033e1f0304d07881
wlcobb <42555289+wlcobb@users.noreply.github.com>
2018-09-13 10:46:50 -0500

    Add ing code to GIT
    The code activity 5 added.

--- activity5.py
+++ activity5.py
@@ -1,2 +1,17 @@
 # Activity 5
-# Erase the current code in the file and create the code to complete the requirements as stated in the assignment.
+
+
+
+#this one uses sets
+def dedupe1(x):                #function to eliminate dups.
+    return list(set(x))
+
+names = ['mary','mary','bill','sam','maria', 'kahn','bill','barry','george','hank','belinda','maria','karthik']
+print ('Activity 5 List Duplicator Function')              #heading for the activity
+print()
+print('Initial List of Names')      #the list of names originally provided
+print (names)
+print()
+my_sorted_list = sorted(dedupe1(names))  #sorting the unduplicated list
+print('List of unique names after running through the deduplicator program')
+print (my_sorted_list)
